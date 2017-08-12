9a7bae9bb73692579be28f1bef27173017c582da
Niclas Lindstedt <nicl@slindstedt.se>
2017-08-12 16:09:33 +0200

    Remove ip and install_dir, generate on run

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,12 @@ SCRIPTFOLDER=$(dirname `readlink -f "$0"`)
 DOCKERFOLDER=$(readlink -f $SCRIPTFOLDER/../)
 port=$(cat $DOCKERFOLDER/conf/docker-port)
 
+ip=$(curl -s https://ifcfg.me/)
+echo "Setting IP to $ip"
+echo $ip > $DOCKERFOLDER/conf/ip
+
+echo -n "Removing old container: "
 docker rm -f nquakesv
-#docker run -d -v $DOCKERFOLDER/conf:/etc/nquakesv --expose $port -p $port:$port --restart always --name nquakesv nquake/server-linux
+
+echo -n "Starting container: "
 docker run -d -v $DOCKERFOLDER/conf:/etc/nquakesv --network=host --restart always --name nquakesv nquake/server-linux

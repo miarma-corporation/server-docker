6fd5d477650beee9fb9b6f631948642cb883f9d6
Niclas Lindstedt <nicl@slindstedt.se>
2017-08-12 23:31:21 +0200

    Add port files for all server types

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,14 +2,20 @@
 
 SCRIPTFOLDER=$(dirname `readlink -f "$0"`)
 DOCKERFOLDER=$(readlink -f $SCRIPTFOLDER/../)
-port=$(cat $DOCKERFOLDER/conf/port)
+mvdsvport=$(cat $DOCKERFOLDER/conf/port-mvdsv)
+qtvport=$(cat $DOCKERFOLDER/conf/port-qtv)
+qwfwdport=$(cat $DOCKERFOLDER/conf/port-qwfwd)
 server=${1:-mvdsv}
 num=${2:-1}
 
 [ "$server" != "mvdsv" ] && [ "$server" != "qtv" ] && [ "$server" != "qwfwd" ] && echo "Invalid server type (available: mvdsv, qtv, qwfwd)." && exit 1
 
+[ "$server" = "mvdsv" ] && port=$mvdsvport && echo "* Using mvdsv configuration"
+[ "$server" = "qtv" ] && port=$qtvport && num=1 && echo "* Using qtv configuration"
+[ "$server" = "qwfwd" ] && port=$qwfwdport && num=1 && echo "* Using qwfwd configuration"
+
 ip=$(curl -s https://ifcfg.me/)
-echo "Setting IP to $ip"
+echo "* Setting IP to $ip"
 echo $ip > $DOCKERFOLDER/conf/ip
 
 running=$(docker ps -a -f name=nquakesv-$server-\* --format "{{.ID}}")
